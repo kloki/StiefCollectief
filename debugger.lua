53c6384172f89ebf7121340b0175def2591eee3f
Koen Klinkers <k.klinkers@gmail.com>
2012-07-30 12:10:40 +0200

    Jumping working correctly now

--- debugger.lua
+++ debugger.lua
@@ -3,6 +3,8 @@ debugger={
    myfont = love.graphics.newFont(love._vera_ttf, 14),
    y=10,
    x=10,
+   yplus=0
+   callbacks={"-","-","-","-","-","-","-"}
    call1=" ",
    call2=" ",
    call3=" ",
@@ -21,21 +23,28 @@ end
 function debugger:draw()
 
    if self.debug then
+      self.yplus=0
       love.graphics.setColor(255,0,0)
       love.graphics.setFont(self.myfont)
       --FPS
-      love.graphics.print("FPS:".. love.timer.getFPS(),self.x,self.y)
+      love.graphics.print("FPS:".. love.timer.getFPS(),self.x,self.y+self.yplus)
+      self.yplus=selyplus+15
       --delta time
-      love.graphics.print("dt:" .. love.timer.getDelta(),self.x,self.y+15)
+      love.graphics.print("dt:" .. love.timer.getDelta(),self.x,self.y+self.yplus)
+      self.yplus=selyplus+15
       --mouse location
-      love.graphics.print("mouseX:".. love.mouse.getX()-theworld.drawx,self.x,self.y+30)
-      love.graphics.print("mouseY:".. love.mouse.getY()-theworld.drawy,self.x,self.y+45)
+      love.graphics.print("mouseX:".. love.mouse.getX()-theworld.drawx,self.x,self.y+self.yplus)
+      self.yplus=selyplus+15
+      love.graphics.print("mouseY:".. love.mouse.getY()-theworld.drawy,self.x,self.y+self.yplus)
+      self.yplus=selyplus+15
       --callbacks
-      love.graphics.print("CollisionCallbacks",self.x,self.y+60)
-      love.graphics.print(" "..self.call1,self.x,self.y+75)
-      love.graphics.print(" "..self.call2,self.x,self.y+90)
-      love.graphics.print(" "..self.call3,self.x,self.y+105)
-      love.graphics.print(" "..self.call4,self.x,self.y+120)
+      love.graphics.print("CollisionCallbacks",self.x,self.y+self.yplus)
+      self.yplus=selyplus+15
+      for i,callback in ipairs(self.callbacks) do
+	 love.graphics.print(" ".. callback,self.x,self.y+self.yplus)
+	 self.yplus=selyplus+15
+      end
+      
       love.graphics.setColor(255,255,255)
    end
 end
@@ -62,8 +71,6 @@ function debugger:on()
 end
 
 function debugger:pushCallback(callback)
-   self.call4=self.call3
-   self.call3=self.call2
-   self.call2=self.call1
-   self.call1=callback
+   table.remove(self.callbacks,#self.callbacks)
+   table.insert(self.callbacks,1,callback)
 end
